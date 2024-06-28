1621aa564591f399144259cdd67395b3c3dc975b
Gabriel Alves Palhares <113656051+GabrielPalhares28@users.noreply.github.com>
2024-06-28 00:03:13 -0300

    Add files via upload

---
+++ cardapio/src/main/java/com/example/cardapio/food/Food.java
@@ -0,0 +1,9 @@
+package com.example.cardapio.food;
+
+import jakarta.persistence.Table;
+import jakarta.perssistence.*Entity
+
+@Table(name = "foods")
+@Entity(name = "foods")
+public class Food {
+}
